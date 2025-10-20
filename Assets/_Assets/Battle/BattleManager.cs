b7365e45becdbb453dfc9a2fccac1d0af52bac0b
Lundy <nlundy@student.uiwtx.edu>
2025-10-20 15:01:17 -0500

    added basic turn logic

--- Assets/_Assets/Battle/BattleManager.cs
+++ Assets/_Assets/Battle/BattleManager.cs
@@ -1,13 +1,19 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BattleManager : MonoBehaviour
 {
     List<BattleSite> mBattleSites;
-    
+    List<BattleCharacter> mBattleCharacters = new List<BattleCharacter>();
+
     public void StartBattle(BattlePartyComponent playerParty, BattlePartyComponent enemyParty)
     {
+        mBattleCharacters.Clear();
         if (mBattleSites == null)
         {
             mBattleSites = new List<BattleSite>();
@@ -17,6 +23,35 @@ public class BattleManager : MonoBehaviour
         Debug.Log($"Starting Battle between: {playerParty.gameObject.name} and {enemyParty.gameObject.name}");
         PrepParty(playerParty);
         PrepParty(enemyParty);
+        StartCoroutine(StartTurns());
+
+    }
+
+    IEnumerator StartTurns()
+    {
+        //TODO: refactor to not hard code the delay
+        yield return new WaitForSeconds(2);
+        NextTurn();
+    }
+
+    void NextTurn()
+    {
+        mBattleCharacters = mBattleCharacters.OrderBy(
+            (battleCharacter) => { return battleCharacter.CooldownTimeRemaining; }).ToList();
+
+        float advanceTime = mBattleCharacters[0].CooldownTimeRemaining;
+        foreach (BattleCharacter battleCharacter in mBattleCharacters)
+        {
+            battleCharacter.AdvanceCooldown(advanceTime);
+        }
+
+        BattleCharacter nextInTurn = mBattleCharacters[0];
+        nextInTurn.TakeTurn();
+
+        mBattleCharacters.Remove(nextInTurn);
+        mBattleCharacters.Add(nextInTurn);
+
+        
     }
 
     private void PrepParty(BattlePartyComponent party)
@@ -32,6 +67,8 @@ public class BattleManager : MonoBehaviour
         {
             partyBattleCharacter.transform.position = partyBattleSite.GetPositionForUnit(i);
             partyBattleCharacter.transform.rotation = partyBattleSite.transform.rotation;
+            partyBattleCharacter.OnTurnFinished += NextTurn;
+            mBattleCharacters.Add(partyBattleCharacter);
             i++;
         }
 
